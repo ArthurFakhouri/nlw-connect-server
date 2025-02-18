98152f5b68e403baef4a20a01bd9c870fb9cfe67
Arthur G. Fakhouri <arthurfakhouri@outlook.com>
2025-02-17 22:44:05 -0300

    new - Creating and setting up our Fastify server and setting up swagger docs

---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { routes } from './routes'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+routes.map(route => {
+  app.register(route)
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('Server Running 🎉')
+})
