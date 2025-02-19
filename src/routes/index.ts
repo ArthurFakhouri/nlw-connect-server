77095e79c0981d816783e9cecdec72ff3b52daf0
Arthur G. Fakhouri <arthurfakhouri@outlook.com>
2025-02-19 14:10:10 -0300

    new: importing all routes created previously

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,15 @@
 import { accessInviteLinkRoute } from './access-invite-link'
+import { getRankingRoute } from './get-ranking'
+import { getSubscriberInviteClicksRoute } from './get-subscriber-invite-clicks'
+import { getSubscriberInvitesCountRoute } from './get-subscriber-invites-count'
+import { getSubscriberRakingPositionRoute } from './get-subscriber-ranking-position'
 import { subscribeToEventRoute } from './subscribe-to-event'
 
-export const routes = [accessInviteLinkRoute, subscribeToEventRoute]
+export const routes = [
+  getRankingRoute,
+  accessInviteLinkRoute,
+  subscribeToEventRoute,
+  getSubscriberInviteClicksRoute,
+  getSubscriberInvitesCountRoute,
+  getSubscriberRakingPositionRoute,
+]
